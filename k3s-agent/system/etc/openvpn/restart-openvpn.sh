959a1c9ca9926dde49794c35029fe39335e788dc
GuojunHou <gjhou@aibee.cn>
2019-12-23 21:14:05 +0800

    test 02

--- k3s-agent/system/etc/openvpn/restart-openvpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-OID=`ps -ef | grep "/usr/sbin/openvpn --daemon" | grep -v grep | wc -l`
-if [ ${OID} -eq 0 ]; then
-/usr/sbin/openvpn --daemon --cd /etc/openvpn --config client.ovpn --log-append /etc/openvpn/openvpn.log
-elif [ ${OID} -ne 1 ]; then
-killall /usr/sbin/openvpn
-/usr/sbin/openvpn --daemon --cd /etc/openvpn --config client.ovpn --log-append /etc/openvpn/openvpn.log
-fi
